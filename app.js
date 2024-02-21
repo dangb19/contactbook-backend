51950e92ea6e673e65d90c87c02fe3815e1368c1
dangb19 <dangb1910053@student.ctu.edu.vn>
2024-02-21 11:32:37 +0700

    Hoan thanh bai TH1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
 
@@ -8,22 +8,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to contact book application.'});
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." });
 });
 
-app.use('/api/contacts', contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
-// handler 404 response
+// Handler 404 response
 app.use((req, res, next) => {
-    return next(new ApiError(404, 'Resource not found'));
+  return next(new ApiError(404, "Resource not found"));
 });
 
 app.use((err, req, res, next) => {
-    return res.status(err.statusCode || 500).json({
-        message: err.message || 'Internal sever error'
-    });
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal sever error",
+  });
 });
 
 module.exports = app;
-
